eb1f565ec0ab3bec1d51e84343d4c28e1f12ec4c
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-09 19:58:50 +0800

    使用全局变量和函数

--- C02/C020301_dataset.py
+++ C02/C020301_dataset.py
@@ -11,9 +11,8 @@
 @Time       :   2019-09-19 17:52
 @License    :   (C)Copyright 2019-2019, zYx.Tom
 @Reference  :   《Python机器学习基础教程》, Sec020301，P24
-@Desc       :   监督学习算法。创建样本数据集
+@Desc       :   监督学习算法。创建样本数据集并展示数据集的效果图
 """
-
 
 # 2.3. 监督学习算法
 import matplotlib.pyplot as plt
@@ -21,12 +20,13 @@ import mglearn
 import numpy as np
 import sklearn
 
-np.set_printoptions(precision = 3, suppress = True, threshold = np.inf)
+from mglearn import datasets
+
 
 # 2.3.1. 一些样本数据集
 # 生成forge数据集，是一个二分类数据集，有两个特征
 def create_forge():
-    X, y = mglearn.datasets.make_forge()
+    X, y = datasets.make_forge()
     mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
     plt.legend(['Class 0', 'Class 1'], loc = 4)
     plt.xlabel('First feature')
@@ -39,7 +39,7 @@ def create_forge():
 
 # 生成wave数据集，只有一个输入特征和一个连续的目标变量（响应）
 def create_wave():
-    X, y = mglearn.datasets.make_wave()
+    X, y = datasets.make_wave()
     mglearn.discrete_scatter(X, y)
     # plt.figure()
     # plt.plot(X, y, '^')
@@ -54,20 +54,20 @@ def create_wave():
 
 # breast cancer 威斯康星州乳腺癌分类数据集，恶性（malignant=0）和良性（benign=1）
 def load_breast_cancer():
-    cancer = sklearn.datasets.load_breast_cancer()
-    print('cancer.keys():\n\t{}'.format(cancer.keys()))
-    print('Shape of cancer data: {}'.format(cancer.data.shape))
+    breast_cancer = sklearn.datasets.load_breast_cancer()
+    print('cancer.keys():\n\t{}'.format(breast_cancer.keys()))
+    print('Shape of cancer data: {}'.format(breast_cancer.data.shape))
     # target_names与target之间的关系是默认设定的
     print('Sample counts per class:\n\t{}'.format(
-            {n: v for n, v in zip(cancer.target_names, np.bincount(cancer.target))}))
+            {n: v for n, v in zip(breast_cancer.target_names, np.bincount(breast_cancer.target))}))
 
     print('Sample counts per class:\n\t{}'.format(
-            list(zip(list(cancer.target_names),
-                     list(np.bincount(cancer.target))))))
+            list(zip(list(breast_cancer.target_names),
+                     list(np.bincount(breast_cancer.target))))))
 
-    print('Feature names:\n{}'.format(cancer.feature_names))
-    print('Shape of cancer feature data: {}'.format(cancer.feature_names.data.shape))
-    return cancer
+    print('Feature names:\n{}'.format(breast_cancer.feature_names))
+    print('Shape of cancer feature data: {}'.format(breast_cancer.feature_names.data.shape))
+    return breast_cancer
 
 
 # boston 波士顿房价回归数据集
@@ -79,6 +79,7 @@ def load_boston():
     # 特征工程（Feature Engineering）：包含了导出特征的方法。
     X, y = mglearn.datasets.load_extended_boston()
     print('X.shape: {}'.format(X.shape))
+
 
 if __name__ == "__main__":
     # 2.3.1. 一些样本数据集
@@ -94,10 +95,6 @@ if __name__ == "__main__":
     # boston 波士顿房价回归数据集
     load_boston()
 
-    import winsound
-    # 运行结束的提醒
-    winsound.Beep(600, 500)
-
-    if len(plt.get_fignums()) != 0:
-        plt.show()
-    pass 
+    import tools
+    tools.beep_end()
+    tools.show_figures()
