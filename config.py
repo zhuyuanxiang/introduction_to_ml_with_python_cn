42d3e0e64af89e3623211fa37d7a4a184b306d97
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-09 19:53:16 +0800

    配置文件，全局变量和全局设置

---
+++ config.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   introduction_to_ml_with_python
+@File       :   config.py
+@Version    :   v0.1
+@Time       :   2020-08-09 17:18
+@License    :   (C)Copyright 2018-2020, zYx.Tom
+@Reference  :   
+@Desc       :   配置文件
+@理解：
+"""
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# ----------------------------------------------------------------------
+plt.rcParams['font.sans-serif'] = ['YaHei Consolas Hybrid']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+# 屏蔽警告：Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX2 FMA
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# 设置数据显示的精确度为小数点后3位
+np.set_printoptions(precision = 3, suppress = True, threshold = np.inf, linewidth = 200)
+# to make this notebook's output stable across runs
+seed = 42
+np.random.seed(seed)
