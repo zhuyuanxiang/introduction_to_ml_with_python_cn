2f9d7a1681f1f132e3e09b4e323189f5b659b68e
zYx.Tom <zhuyuanxiang@gmail.com>
2020-08-28 15:32:00 +0800

    提取数据生成函数： load_train_test_breast_cancer() load_train_test_wave() make_my_forge()
    重新格式化代码
    输出中文化

--- C02/C020302_K_neighbors.py
+++ C02/C020302_K_neighbors.py
@@ -16,11 +16,14 @@
 
 # 2.3. 监督学习算法
 import matplotlib.pyplot as plt
-import numpy as np
 import mglearn
-import sklearn
+import numpy as np
 
-from mglearn import datasets
+from config import seed
+from datasets.load_data import load_train_test_breast_cancer
+from datasets.load_data import load_train_test_wave
+from datasets.load_data import make_my_forge
+from tools import show_title
 
 
 # 2.3.2. k-NN（k近邻）
@@ -29,60 +32,47 @@ def compare_NeighborsNumber():
     # 左上角的数据点和n_neighbors=1时不同
     for n_neighbors in [1, 3, 5, 7]:
         plt.figure()
-        mglearn.plots.plot_knn_classification(n_neighbors = n_neighbors)
-        plt.suptitle("图2-5：{}近邻模型对forge数据集的预测结果".format(n_neighbors))
+        mglearn.plots.plot_knn_classification(n_neighbors=n_neighbors)
+        plt.suptitle(f"图2-5：{n_neighbors}近邻模型对forge数据集的预测结果")
         pass
 
 
 # 训练K近邻分类模型
-def make_my_forge():
-    # a carefully hand-designed dataset lol
-    X, y = sklearn.datasets.make_blobs(centers = 2, random_state = 4, n_samples = 30)
-    y[np.array([7, 27])] = 0
-    mask = np.ones(len(X), dtype = np.bool)
-    mask[np.array([0, 1, 5, 26])] = 0
-    X, y = X[mask], y[mask]
-    return X, y
-
-
 def fit_KNeighborsClassifier():
     # 准备forge数据集(二分类问题)，数据越多越准确
-    X, y = datasets.make_forge()
+    # X, y = datasets.make_forge()
     X, y = make_my_forge()
-    plt.scatter(X[:, 0], X[:, 1], y, cmap = mglearn.cm2, marker = '*')
+    plt.scatter(X[:, 0], X[:, 1], y, cmap=mglearn.cm2, marker='*')
 
     from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = config.seed)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
 
     from sklearn.neighbors import KNeighborsClassifier
     for n_neighbors in [1, 3]:
-        clf = KNeighborsClassifier(n_neighbors = n_neighbors)
+        clf = KNeighborsClassifier(n_neighbors=n_neighbors)
         clf.fit(X_train, y_train)
-        print('-' * 5 + '>n_neighbors={}<'.format(n_neighbors) + '-' * 5)
-        print('Test set predictions: {}'.format(clf.predict(X_test)))
-        print('Test set accuracy: {:.2f}'.format(clf.score(X_test, y_test)))
+        show_title(f'邻居个数={n_neighbors}')
+        print('测试集预测: {}'.format(clf.predict(X_test)))
+        print('测试集精度: {:.2f}'.format(clf.score(X_test, y_test)))
 
 
 # 2) 分析 KNeighborsClassifier() 函数
 # 不同n_neighbors值的K近邻模型的决策边界
 def analysis_KNeighborsClassifier():
     # 准备forget数据集，数据越多越准确
-    from sklearn.model_selection import train_test_split
-    X, y = mglearn.datasets.make_forge()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = config.seed)
-
+    X, y = make_my_forge()
     # 邻居数越少，决策边界越受每一个数据的特性影响；
     # 邻居数越大，决策边界越受所有数据的平均特性影响，即决策边界会越平滑。
     from sklearn.neighbors import KNeighborsClassifier
-    fig, axes = plt.subplots(1, 3, figsize = (10, 3))
-    plt.suptitle("图2-6：不同n_neighbors值的K近邻模型的决策边界")
+    fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+    plt.suptitle("图2-6：不同邻居个数的K近邻模型的决策边界")
     for n_neighbors, ax in zip([1, 3, 9], axes):
-        clf = KNeighborsClassifier(n_neighbors = n_neighbors).fit(X, y)
-        mglearn.plots.plot_2d_separator(clf, X, fill = True, eps = 0.5, ax = ax, alpha = .4)
-        mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax = ax)
-        ax.set_title('{} neighbor(s)'.format(n_neighbors))
-        ax.set_xlabel('feature 0')
-        ax.set_ylabel('feature 1')
+        clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+        mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+        mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+        ax.set_title(f'{n_neighbors} 个邻居')
+        ax.set_xlabel('特征 0')
+        ax.set_ylabel('特征 1')
 
     # 不同的位置显示图例：
     axes[0].legend()
@@ -102,10 +92,7 @@ def analysis_KNeighborsClassifier():
 
 # 以n_neighbors为自变量，对比训练集精度和测试集精度
 def analysis_ModelComplexity():
-    from sklearn.model_selection import train_test_split
-    cancer = sklearn.datasets.load_breast_cancer()
-    X_train, X_test, y_train, y_test = train_test_split(
-            cancer.data, cancer.target, stratify = cancer.target, random_state = 66)
+    X_test, X_train, y_test, y_train = load_train_test_breast_cancer()
     training_accuracy = []
     test_accuracy = []
     neighbors_settings = range(1, 11)
@@ -115,14 +102,14 @@ def analysis_ModelComplexity():
     # 当达到顶点后，模型泛化能力开始下降，测试数据的精确度也开始下降，模型过拟合。
     from sklearn.neighbors import KNeighborsClassifier
     for n_neighbors in neighbors_settings:
-        clf = KNeighborsClassifier(n_neighbors = n_neighbors).fit(X_train, y_train)
+        clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X_train, y_train)
         training_accuracy.append(clf.score(X_train, y_train))  # 记录训练精度
         test_accuracy.append((clf.score(X_test, y_test)))  # 记录测试精度
 
-    plt.plot(neighbors_settings, training_accuracy, label = 'training accuracy')
-    plt.plot(neighbors_settings, test_accuracy, label = 'test accuracy')
-    plt.xlabel('n_neighbors')
-    plt.ylabel('Accuracy')
+    plt.plot(neighbors_settings, training_accuracy, label='训练精度')
+    plt.plot(neighbors_settings, test_accuracy, label='测试精度')
+    plt.xlabel('邻居个数')
+    plt.ylabel('精度')
     plt.legend()
     plt.suptitle("图2-7：以n_neighbors为自变量，对比训练集精度和测试集精度")
 
@@ -131,20 +118,18 @@ def analysis_ModelComplexity():
 def compare_KNeighborsRegressor():
     # 左边第1个数据预测值变化较大
     for n_neighbors in [1, 3, 5, 7]:
-        mglearn.plots.plot_knn_regression(n_neighbors = n_neighbors)
-        plt.suptitle("图2-8：{}近邻回归对wave数据集的预测结果".format(n_neighbors))
+        mglearn.plots.plot_knn_regression(n_neighbors=n_neighbors)
+        plt.suptitle(f"图2-8：{n_neighbors}近邻回归对wave数据集的预测结果")
         pass
 
 
 def fit_KNeighborsRegressor():
-    from sklearn.model_selection import train_test_split
-    X, y = mglearn.datasets.make_wave(n_samples = 40)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = config.seed)
+    X_test, X_train, y_test, y_train = load_train_test_wave()
     from sklearn.neighbors import KNeighborsRegressor
     for n_neighbors in [1, 3, 5, 7]:
-        regress = KNeighborsRegressor(n_neighbors = n_neighbors).fit(X_train, y_train)
-        print('n_neighbors: {}'.format(n_neighbors))
-        print('Test set predictions:\n{}'.format(regress.predict(X_test)))
+        regress = KNeighborsRegressor(n_neighbors=n_neighbors).fit(X_train, y_train)
+        print(f'邻居个数: {n_neighbors}')
+        print(f'测试集的预测结果:\n{regress.predict(X_test)}')
         # R^2分数，叫做决定系数，是回归模型预测的优度度量。
         '''The coefficient R^2 is defined as (1 - u/v), 
         where u is the residual sum of squares ((y_true - y_pred) ** 2).sum() 
@@ -153,33 +138,30 @@ def fit_KNeighborsRegressor():
         model can be arbitrarily worse). 
         A constant model that always predicts the expected value of y, 
         disregarding the input features, would get a R^2 score of 0.0.'''
-        print('Test set R^2: {:.2f}'.format(regress.score(X_test, y_test)))
+        print('测试集的分类 R^2: {:.2f}'.format(regress.score(X_test, y_test)))
         print('-----')
 
 
 # 4) 分析 KNeighborsRegressor() 函数
 def analysis_KNeighborsRegressor():
-    from sklearn.model_selection import train_test_split
-    X, y = mglearn.datasets.make_wave(n_samples = 40)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = config.seed)
-    fig, axes = plt.subplots(2, 3, figsize = (15, 8))
+    X_test, X_train, y_test, y_train = load_train_test_wave()
+    fig, axes = plt.subplots(2, 3, figsize=(15, 8))
     line = np.linspace(-3, 3, 1000).reshape(-1, 1)
     print(line.data.shape)
 
     from sklearn.neighbors import KNeighborsRegressor
     for n_neighbors, ax in zip([1, 3, 5, 7, 9, 11], axes.reshape(6)):
-        reg = KNeighborsRegressor(n_neighbors = n_neighbors).fit(X_train, y_train)
+        reg = KNeighborsRegressor(n_neighbors=n_neighbors).fit(X_train, y_train)
         ax.plot(line, reg.predict(line))
-        ax.plot(X_train, y_train, '^', c = mglearn.cm2(0), markersize = 8)
-        ax.plot(X_test, y_test, 'v', c = mglearn.cm2(1), markersize = 8)
-        ax.set_title('{} neighbor(s)\n train score: {:.2f} test score: {:.2f}'.format(
+        ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+        ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+        ax.set_title('{} 个邻居： 训练得分{:.2f} 测试得分{:.2f}'.format(
                 n_neighbors, reg.score(X_train, y_train), reg.score(X_test, y_test)))
-        ax.set_xlabel('Feature')
-        ax.set_ylabel('Target')
+        ax.set_xlabel('特征')
+        ax.set_ylabel('目标')
 
-    axes[0, 0].legend(['Model predictions', 'Training data/target',
-                       'Test data/target'], loc = 'best')
-    plt.suptitle("图2-10：不同n_neighbors值的K近邻回归的预测结果对比")
+    axes[0, 0].legend(['模型预测', '训练集', '测试集'], loc='best')
+    plt.suptitle("图2-10：不同邻居个数的K近邻回归的预测结果对比\n邻居个数越大，回归曲线越平滑")
 
 
 if __name__ == "__main__":
@@ -207,7 +189,8 @@ if __name__ == "__main__":
     # 4) 分析 KNeighborsRegressor() 函数
     analysis_KNeighborsRegressor()
 
-    import tools
+    from tools import beep_end
+    from tools import show_figures
 
-    tools.beep_end()
-    tools.show_figures()
+    beep_end()
+    show_figures()
