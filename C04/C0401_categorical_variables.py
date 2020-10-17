90a59be1d2dddcfcecc621a68cf6e083689fda61
zYx.Tom <zhuyuanxiang@gmail.com>
2020-10-17 09:56:26 +0800

    配置参数统一到 preamble.py 中 调整输入格式，方便阅读

--- C04/C0401_categorical_variables.py
+++ C04/C0401_categorical_variables.py
@@ -13,37 +13,15 @@
 @Reference  :   《Python机器学习基础教程》, Sec0401，P161
 @Desc       :   数据表示与特征工程。分类变量。
 """
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-# 设置数据显示的精确度为小数点后3位
-np.set_printoptions(precision = 3, suppress = True, threshold = np.inf)
-# -------
-# 显示所有列，默认为5
-pd.set_option('display.max_columns', None)
-# -------
-# deprecated，使用display_max_rows代替
-# pd.set_option('display.height',1000)
-# 显示所有行，默认为5
-pd.set_option('display.max_rows', None)
-# -------
-# 设置value的显示长度为100，默认为50
-pd.set_option('max_colwidth', 500)
-# -------
-# 设置显示的宽度，默认为80
-pd.set_option('display.width', 1000)
+from datasets.load_data import load_adult_data
+from tools import *
 
 
 # 4.1.1 One-Hot编码（虚拟变量）
 def one_hot_encode():
-    read_data = pd.read_csv('../data/adult.data', header = None, index_col = False,
-                            names = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
-                                     'marital-status', 'occupation', 'relationship', 'race', 'gender',
-                                     'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income'])
+    read_data = load_adult_data()
     data = read_data[['age', 'workclass', 'education', 'gender', 'hours-per-week', 'occupation', 'income']]
-    print("-- data的前五个数据内容 --")
-    print('=' * 20)
+    show_title("data的前五个数据内容")
     print(data.head())
 
     # 1. 检查字符串编码的分类数据
@@ -62,28 +40,27 @@ def one_hot_encode():
     # 组合的特征太多，只显示其中的5个
     pd.set_option('display.max_columns', 5)
     print('-' * 20)
-    print("data_dummies的前五个数据内容：")
+    print("data_dummies的前五条数据的内容：")
     print(data_dummies.head())
 
     # .ix is deprecated. Please use .loc for label based indexing or .iloc for positional indexing
     # features = data_dummies.ix[:, 'age':'occupation_Transport-moving']
-    print('-' * 20)
-    print("-- 提取包含特征的列 --")
+    show_title("提取包含特征的列")
     print("从age到occupation_ Transport-moving的所有列")
     features = data_dummies.loc[:, 'age':'occupation_ Transport-moving']
     X = features.values  # 转化为Numpy数组
     y = data_dummies['income_ >50K'].values
     print('X.shape(): {}, y.shape(): {}'.format(X.shape, y.shape))
-    print('X=', X[:5])
+    print('X=\n', X[:5])
     print('y=', y[:5])
-    bincount = np.bincount(y)
-    print('np.bincount(y) = 0的个数 {} 和 1的个数 {}', bincount[0], bincount[1])
+    bin_count = np.bincount(y)
+    print('np.bin_count(y) = (0的个数 {}) 和 (1的个数 {})'.format(bin_count[0], bin_count[1]))
 
     from sklearn.linear_model import LogisticRegression
     from sklearn.model_selection import train_test_split
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
-    log_reg = LogisticRegression(solver = 'lbfgs', max_iter = 10000)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
+    log_reg = LogisticRegression(solver='lbfgs', max_iter=10000)
     log_reg.fit(X_train, y_train)
     print('=' * 20)
     print('基于LogisticRegression学习的测试集评价: {:.2f}'.format(log_reg.score(X_test, y_test)))
@@ -93,22 +70,28 @@ def one_hot_encode():
 
 # 4.1.2. 数字可以编码分类变量
 def demo_dataframe():
-    demo_df = pd.DataFrame({'Integer Feature': [0, 1, 2, 1], 'Categorical Feature': ['socks', 'fox', 'socks', 'box']})
+    demo_df = pd.DataFrame({'Categorical Feature': ['socks', 'fox', 'socks', 'box'], 'Integer Feature': [0, 1, 2, 1]})
     demo_df_str = demo_df.copy()
     demo_df_str['Integer Feature'] = demo_df['Integer Feature'].astype(str)  # 转换成str类型
     demo_df_dummies = pd.get_dummies(demo_df)
     demo_df_str_dummies = pd.get_dummies(demo_df_str)
-    print("表4-4：包含分类字符串特征和整数特征的数据框")
+    show_title("表4-4：包含分类字符串特征和整数特征的数据框")
+    print('-' * 40)
     print(demo_df)
     print('-' * 40)
-    print("表4-4-str：包含分类字符串特征和整数字符串特征的数据框")
+    show_title("表4-4-str：包含分类字符串特征和整数字符串特征的数据框")
+    print('-' * 40)
     print(demo_df_str)
     print('-' * 40)
-    print("表4-5：表4-4中数据的One-Hot编码版本，整数特征不变")
+    show_title("表4-5：表4-4中数据的One-Hot编码版本，整数特征不变")
+    print('-' * 40)
     print(demo_df_dummies)
     print('-' * 40)
-    print("表4-5：表4-4-str中数据的One-Hot编码版本，整数字符串特征也同时编码")
+    show_title("表4-5：表4-4-str中数据的One-Hot编码版本，整数字符串特征也同时编码")
+    print('-' * 40)
     print(demo_df_str_dummies)
+    print('-' * 40)
+    show_title("编码后的数据列表")
     from tabulate import tabulate
     print(tabulate(demo_df_str_dummies))
 
@@ -117,11 +100,5 @@ if __name__ == "__main__":
     # 4.1.1 One-Hot编码（虚拟变量）
     # one_hot_encode()
     demo_dataframe()
-
-    import winsound
-
-    # 运行结束的提醒
-    winsound.Beep(600, 500)
-    if len(plt.get_fignums()) != 0:
-        plt.show()
-    pass
+    beep_end()
+    show_figures()
