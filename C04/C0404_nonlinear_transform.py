f5d8b92348141194fd6c30b077ae18567da0aa7e
zYx.Tom <zhuyuanxiang@gmail.com>
2020-10-18 10:56:52 +0800

    配置参数统一到 preamble.py 中 调整输出格式，方便阅读

--- C04/C0404_nonlinear_transform.py
+++ C04/C0404_nonlinear_transform.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-    
+# -*- encoding: utf-8 -*-
 """
 @Author     :   zYx.Tom
 @Contact    :   526614962@qq.com
@@ -13,29 +13,28 @@
 @Reference  :   《Python机器学习基础教程》, Sec0404，P178
 @Desc       :   数据表示与特征工程。单变量非线性变换。
 """
-import matplotlib.pyplot as plt
-import numpy as np
+from tools import *
 
-# 设置数据显示的精确度为小数点后3位
-np.set_printoptions(precision = 3, suppress = True, threshold = np.inf, linewidth = 200)
 
 # 4.4. 单变量非线性变换
 def single_variable_nonlinear_transform():
     rnd = np.random.RandomState(0)
-    X_org = rnd.normal(size = (1000, 3))
-    w = rnd.normal(size = 3)
+    X_org = rnd.normal(size=(100000, 3))
+    w = rnd.normal(size=3)
 
-    # ToDo: 这个转换不理解，为什么先得到一个正态分布，再转换成泊松分布呢？
+    # 因为泊松分布与正态分布之间的关系，因此使用正态分布建立泊松分布
+    # 二项分布、泊松分布与正态分布之间的关系可以参考：
+    # https://hongyitong.github.io/2016/11/13/%E4%BA%8C%E9%A1%B9%E5%88%86%E5%B8%83%E3%80%81%E6%B3%8A%E6%9D%BE%E5%88%86%E5%B8%83%E3%80%81%E6%AD%A3%E6%80%81%E5%88%86%E5%B8%83/
     X = rnd.poisson(10 * np.exp(X_org))
     y = np.dot(X_org, w)
 
-    print('Number of feature appearances:\n{}'.format(np.bincount(X[:, 0])))
-
     bins = np.bincount(X[:, 0])
+    print('特征表示数目统计：\n{}'.format(bins[:40]))
+
     plt.figure()
     plt.bar(range(len(bins)), bins)
-    plt.xlabel('Value')
-    plt.ylabel('Number of appearances')
+    plt.xlabel('值')
+    plt.ylabel('特征表示的数目')
     plt.suptitle("图4-7：X[:,0]特征取值的直方图")
 
     # 这样的泊松分布数据很难处理！
@@ -43,36 +42,29 @@ def single_variable_nonlinear_transform():
     from sklearn.linear_model import Ridge
     from sklearn.model_selection import train_test_split
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
 
-    print('-' * 20)
-    number_title = "泊松分布数据使用岭回归模型来拟合"
-    print('-' * 5, number_title, '-' * 5)
-    print('Test score: {:.3f}'.format(Ridge().fit(X_train, y_train).score(X_test, y_test)))
+    show_title("泊松分布数据使用岭回归模型来拟合")
+    print('测试集的得分 {:.3f}'.format(Ridge().fit(X_train, y_train).score(X_test, y_test)))
 
     # 应用对数变换后，可能有用
     X_train_log = np.log(X_train + 1)
     X_test_log = np.log(X_test + 1)
 
     plt.figure()
-    plt.hist(X_train_log[:, 0], bins = 25)
-    plt.xlabel('Value')
-    plt.ylabel('Number of appearances')
+    plt.hist(X_train_log[:, 0], bins=25)
+    plt.xlabel('值')
+    plt.ylabel('特征表示的数目')
     plt.suptitle("图4-8：对X[:,0]特征取值进行对数变换后的直方图")
 
     # 再使用岭回归模型学习数据，拟合效果变好。（理解：将数据转换为高斯分布，因为岭回归就是基于L2正则的最小二乘法）
-    print('-' * 20)
-    print("应用对数变换后的泊松分布数据使用岭回归模型来拟合。")
-    print('Test score: {:.3f}'.format(Ridge().fit(X_train_log, y_train).score(X_test_log, y_test)))
+    show_title("应用对数变换后的泊松分布数据使用岭回归模型来拟合")
+    print('测试集的得分 {:.3f}'.format(Ridge().fit(X_train_log, y_train).score(X_test_log, y_test)))
 
     pass
 
+
 if __name__ == "__main__":
     single_variable_nonlinear_transform()
-    import winsound
-
-    # 运行结束的提醒
-    winsound.Beep(600, 500)
-    if len(plt.get_fignums()) != 0:
-        plt.show()
-    pass 
+    beep_end()
+    show_figures()
