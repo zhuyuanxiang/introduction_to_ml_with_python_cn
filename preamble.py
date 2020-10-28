b3eac224f1d0e970a51776d8ef8e711964590648
zYx.Tom <zhuyuanxiang@gmail.com>
2020-10-28 18:09:10 +0800

    增加 pandas 设置参数 增加 warnings 设置，过滤 warning 在输出窗口中报警

--- preamble.py
+++ preamble.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import warnings
 from IPython.display import display
 from IPython.display import set_matplotlib_formats
 from cycler import cycler
@@ -34,7 +35,23 @@ plt.rc('axes', prop_cycle=(
 # 设置数据显示的精确度为小数点后3位
 np.set_printoptions(precision=3, suppress=True, threshold=np.inf, linewidth=200)
 
-pd.set_option("display.max_columns", 8)
-pd.set_option('precision', 2)
+pd.set_option('precision', 3)
+# -------
+# 显示所有列，默认为5
+pd.set_option('display.max_columns', None)
+# pd.set_option("display.max_columns", 8)
+# -------
+# deprecated，使用display_max_rows代替
+# pd.set_option('display.height',1000)
+# 显示所有行，默认为5
+pd.set_option('display.max_rows', None)
+# -------
+# 设置value的显示长度为100，默认为50
+pd.set_option('max_colwidth', 500)
+# -------
+# 设置显示的宽度，默认为80
+pd.set_option('display.width', 1000)
+
+warnings.filterwarnings('ignore')
 
 __all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
