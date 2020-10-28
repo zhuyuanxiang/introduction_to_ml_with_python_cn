09828fd97f47c87dcbe390fff5d16764eba49a17
zYx.Tom <zhuyuanxiang@gmail.com>
2020-10-28 18:03:34 +0800

    格式化代码，方便阅读

--- mglearn/plot_grid_search.py
+++ mglearn/plot_grid_search.py
@@ -1,41 +1,36 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.datasets import load_iris
+import numpy as np
 import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.svm import SVC
 
 
 def plot_cross_val_selection():
     iris = load_iris()
-    X_trainval, X_test, y_trainval, y_test = train_test_split(iris.data,
-                                                              iris.target,
-                                                              random_state=0)
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        iris.data, iris.target, random_state=0)
 
     param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100],
                   'gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
-    grid_search = GridSearchCV(SVC(), param_grid, cv=5,
-                               return_train_score=True)
-    grid_search.fit(X_trainval, y_trainval)
-    results = pd.DataFrame(grid_search.cv_results_)[15:]
+    grid_search = GridSearchCV(SVC(), param_grid, cv=5, return_train_score=True)
+    grid_search.fit(X_train_val, y_train_val)
+    results = pd.DataFrame(grid_search.cv_results_)
 
     best = np.argmax(results.mean_test_score.values)
     plt.figure(figsize=(10, 3))
     plt.xlim(-1, len(results))
     plt.ylim(0, 1.1)
+    marker_cv, marker_mean, marker_best = None, None, None
     for i, (_, row) in enumerate(results.iterrows()):
         scores = row[['split%d_test_score' % i for i in range(5)]]
-        marker_cv, = plt.plot([i] * 5, scores, '^', c='gray', markersize=5,
-                              alpha=.5)
-        marker_mean, = plt.plot(i, row.mean_test_score, 'v', c='none', alpha=1,
-                                markersize=10, markeredgecolor='k')
+        marker_cv, = plt.plot([i] * 5, scores, '^', c='gray', markersize=5, alpha=.5)
+        marker_mean, = plt.plot(i, row.mean_test_score, 'v', c='none', alpha=1, markersize=10, markeredgecolor='k')
         if i == best:
-            marker_best, = plt.plot(i, row.mean_test_score, 'o', c='red',
-                                    fillstyle="none", alpha=1, markersize=20,
-                                    markeredgewidth=3)
+            marker_best, = plt.plot(i, row.mean_test_score, 'o', c='red', fillstyle="none", alpha=1, markersize=20, markeredgewidth=3)
 
-    plt.xticks(range(len(results)), [str(x).strip("{}").replace("'", "") for x
-                                     in grid_search.cv_results_['params']],
+    plt.xticks(range(len(results)),
+               [str(x).strip("{}").replace("'", "") for x in grid_search.cv_results_['params']],
                rotation=90)
     plt.ylabel("Validation accuracy")
     plt.xlabel("Parameter settings")
