6d534432768261ab789b742e9c3e4c0c73da16c6
zYx.Tom <zhuyuanxiang@gmail.com>
2020-10-30 16:56:10 +0800

    C06 代码简化，输出中文化，

--- C06/C06_pipeline.py
+++ C06/C06_pipeline.py
@@ -13,56 +13,43 @@
 @Reference  :   《Python机器学习基础教程》, Ch06, P236
 @Desc       :   算法链与管道
 """
+from datasets.load_data import load_train_test_boston
+from datasets.load_data import load_train_test_breast_cancer
 from tools import *
 
 
 # 使用预处理数据训练 SVM 模型
 def svm_with_preprocessing_data():
     """训练集中的数据需要进行预处理"""
+    X_train, X_test, y_train, y_test = load_train_test_breast_cancer()
 
-    # 加载然后划分数据
-    from sklearn.datasets import load_breast_cancer
-    cancer = load_breast_cancer()
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
-
-    # 对训练数据集进行缩放
+    # 对 训练数据集 进行 0-1 缩放 实现 数据预处理
     from sklearn.preprocessing import MinMaxScaler
-    # 使用0-1缩放进行预处理的数据
     scaler = MinMaxScaler().fit(X_train)
 
     X_train_scaled = scaler.transform(X_train)
     X_test_scaled = scaler.transform(X_test)
 
-    # 在缩放前的训练数据集上学习SVM
+    # 在缩放前的 训练数据集 上学习 SVM
     from sklearn.svm import SVC
     svm = SVC(gamma='auto')
     svm.fit(X_train, y_train)
-    number_title = "在缩放前的测试数据集上评估SVM"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    show_title("在缩放前的 测试数据集 上评估 SVM")
     print("Test score: {:.2f}".format(svm.score(X_test, y_test)))
 
-    # 在缩放后的训练数据集上学习SVM
+    # 在缩放后的 训练数据集 上学习 SVM
     from sklearn.svm import SVC
     svm = SVC(gamma='auto')
     svm.fit(X_train_scaled, y_train)
-    number_title = "在缩放后的测试数据集上评估SVM"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    show_title("在缩放后的 测试数据集 上评估 SVM")
     print("Test score: {:.2f}".format(svm.score(X_test_scaled, y_test)))
 
 
 # 6.1 用预处理进行参数选择
 def parameter_selection():
     # 加载然后划分数据
-    from sklearn.datasets import load_breast_cancer
-    cancer = load_breast_cancer()
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
-
-    number_title = "不使用管道只基于预处理进行参数选择"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    X_train, X_test, y_train, y_test = load_train_test_breast_cancer()
+    show_title("不使用管道进行参数选择")
 
     # 对训练数据集进行缩放
     # 使用全部训练数据集的信息进行缩放学习，
@@ -76,21 +63,23 @@ def parameter_selection():
     X_train_scaled = scaler.transform(X_train)
     X_test_scaled = scaler.transform(X_test)
 
-    param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100], 'gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
-
     # 在缩放后的训练数据集上学习SVM
     from sklearn.svm import SVC
     from sklearn.model_selection import GridSearchCV
+    param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100], 'gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
     grid_search = GridSearchCV(SVC(), param_grid=param_grid, cv=5)
     grid_search.fit(X_train_scaled, y_train)
-    print("Best cross-validation accuracy: {:.2f}".format(grid_search.best_score_))
-    print("Best test set score: {:.2f}".format(grid_search.score(X_test_scaled, y_test)))
-    print("Best parameters: ", grid_search.best_params_)
-    print("SVM estimator: ", grid_search.best_estimator_)
+    print("验证集上的最佳精度 (Best cross-validation accuracy): {:.2f}".format(grid_search.best_score_))
+    print("验证集上的最佳参数 (Best parameters): ", grid_search.best_params_)
+    print("验证集上的最佳估计器 (SVM estimator): ", grid_search.best_estimator_)
+    print("测试集上的精度 (Best test set score): {:.2f}".format(grid_search.score(X_test_scaled, y_test)))
 
+
+def plot_data_preprocess():
     # 交叉验证与最终评估这两个过程中数据处理存在的不同
     mglearn.plots.plot_improper_processing()
-    plt.suptitle("图6-1：在交叉验证循环外部对数据进行预处理时的数据使用情况")
+    plt.suptitle("图6-1：在交叉验证循环外部对数据进行预处理时的数据使用情况\n"
+                 "因为显示尺度问题，建议看书上的图更准确")
 
 
 # 6.2 构建管道
@@ -99,14 +88,8 @@ def parameter_selection():
 # Pipeline最常见的用例是将预处理步骤（比如：数据缩放）与一个监督模型（比如：分类器）链接在一起
 def construct_pipeline():
     # 加载然后划分数据
-    from sklearn.datasets import load_breast_cancer
-    cancer = load_breast_cancer()
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
-
-    number_title = "使用管道实现数据预处理和模型训练"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    X_train, X_test, y_train, y_test = load_train_test_breast_cancer()
+    show_title("使用管道基于固定参数实现数据预处理和模型训练")
 
     # 对训练数据集进行缩放
     # 使用全部训练数据集的信息进行缩放学习，
@@ -119,7 +102,7 @@ def construct_pipeline():
     from sklearn.pipeline import Pipeline
     # 创建了两个步骤：数据预处理（0-1缩放）和监督模型（SVM）
     # Current default is 'auto' which uses 1 / n_features
-    pipe_line = Pipeline([('scaler', MinMaxScaler()), ("svm", SVC(gamma='auto'))])  # SVC(gamma = 1, C = 1))])
+    pipe_line = Pipeline([('scaler', MinMaxScaler()), ("svm", SVC(C=1.0, gamma='auto'))])  # SVC(gamma = 1, C = 1))])
     pipe_line.fit(X_train, y_train)
     print("Test score: {:.2f}".format(pipe_line.score(X_test, y_test)))
 
@@ -131,14 +114,8 @@ def construct_pipeline():
 # 6.3 在网格搜索中使用管道
 def pipeline_in_grid_search():
     # 加载然后划分数据
-    from sklearn.datasets import load_breast_cancer
-    cancer = load_breast_cancer()
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
-
-    number_title = "在网格搜索中使用管道"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    X_train, X_test, y_train, y_test = load_train_test_breast_cancer()
+    show_title("使用管道进行参数选择")
 
     # 对训练数据集进行缩放
     # 使用全部训练数据集的信息进行缩放学习，
@@ -155,13 +132,17 @@ def pipeline_in_grid_search():
     pipe_line.fit(X_train, y_train)
 
     # 在网格搜索中使用管道
-    param_grid = {'svm__C': [0.001, 0.01, 0.1, 1, 10, 100], 'svm__gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
     from sklearn.model_selection import GridSearchCV
-    grid = GridSearchCV(pipe_line, param_grid=param_grid, cv=5)
-    grid.fit(X_train, y_train)
-    print("Best cross-validation accuracy: {:.2f}".format(grid.best_score_))
-    print("Best set score: {:.2f}".format(grid.score(X_test, y_test)))
-    print("Best parameters: ", grid.best_params_)
+    param_grid = {'svm__C': [0.001, 0.01, 0.1, 1, 10, 100], 'svm__gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
+    grid_search = GridSearchCV(pipe_line, param_grid=param_grid, cv=5)
+    grid_search.fit(X_train, y_train)
+    print("验证集上的最佳精度 (Best cross-validation accuracy): {:.2f}".format(grid_search.best_score_))
+    print("验证集上的最佳参数 (Best parameters): ", grid_search.best_params_)
+    print("验证集上的最佳估计器 (SVM estimator): ", grid_search.best_estimator_)
+    print("测试集上的精度 (Best test set score): {:.2f}".format(grid_search.score(X_test, y_test)))
+
+
+def plot_inner_preprocess():
     # 输出的结果与图6-1处理的结果没有区别
     # 因为在交叉验证中，信息泄漏的影响取决于预处理步骤的性质。
     # 如果是使用测试部分来估计数据的范围，一般的影响不大；
@@ -197,13 +178,13 @@ def information_leak():
     from sklearn.linear_model import Ridge
     print('-' * 20)
     print("Cross-validation accuracy(cv only on ridge): {:.2f}".format(
-        np.mean(cross_val_score(Ridge(),
-                                X_selected, y, cv=5))
+            np.mean(cross_val_score(Ridge(),
+                                    X_selected, y, cv=5))
     ))
     from sklearn.svm import SVR
     print("Cross-validation accuracy(cv only on svm): {:.2f}".format(
-        np.mean(cross_val_score(SVR(kernel='rbf', gamma='scale', C=1.0, epsilon=0.2),
-                                X_selected, y, cv=5))
+            np.mean(cross_val_score(SVR(kernel='rbf', gamma='scale', C=1.0, epsilon=0.2),
+                                    X_selected, y, cv=5))
     ))
 
     from sklearn.pipeline import Pipeline
@@ -214,66 +195,71 @@ def information_leak():
     pipe_line = Pipeline([('select', SelectPercentile(score_func=f_regression, percentile=5)),
                           ("ridge", Ridge())])
     print("Cross-validation accuracy (pipeline on ridge): {:.2f}".format(
-        np.mean(cross_val_score(pipe_line, X, y, cv=5))
+            np.mean(cross_val_score(pipe_line, X, y, cv=5))
     ))
     # 创建了两个步骤：数据预处理（0-1缩放）和监督模型（SVR）
     pipe_line = Pipeline([('select', SelectPercentile(score_func=f_regression, percentile=5)),
                           ("svm", SVR(kernel='rbf', gamma='scale', C=1.0, epsilon=0.2))])
     print("Cross-validation accuracy (pipeline on svm): {:.2f}".format(
-        np.mean(cross_val_score(pipe_line, X, y, cv=5))
+            np.mean(cross_val_score(pipe_line, X, y, cv=5))
     ))
     pass
 
 
 # 6.4 通用的管道接口
+def fit(self, X, y):
+    X_transformed = X
+    for name, estimator in self.steps[:-1]:
+        # 遍历除最后一步之外的所有步骤对数据进行拟合和变换
+        X_transformed = estimator.fit_transform(X_transformed, y)
+    # 对最后一步进行拟合
+    self.steps[-1][1].fit(X_transformed, y)
+    return self
+
+
+def predict(self, X):
+    X_transformed = X
+    for step in self.steps[:-1]:
+        # 遍历除最后一步之外的所有步骤对数据进行变换
+        X_transformed = step[1].transform(X_transformed)
+    # 利用最后一步进行预测
+    return self.steps[-1][1].predict(X_transformed)
+
+
 # 6.4.1 使用make_pipeline创建管道
 # 6.4.2 访问管道中某个步骤的属性
 def create_pipeline_methods():
     from sklearn.preprocessing import MinMaxScaler
     from sklearn.svm import SVC
 
-    number_title = "创建管道"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    show_title("创建管道")
 
     # 创建Pipeline的标准语法
     from sklearn.pipeline import Pipeline
     pipeline_long = Pipeline([('scaler', MinMaxScaler()), ('svm', SVC(C=100))])
-    print('=' * 20)
     print("Long pipeline steps: {}".format(pipeline_long.steps))
 
     # 创建Pipeline的缩写语法
     from sklearn.pipeline import make_pipeline
-    pipeline_short = make_pipeline(MinMaxScaler, SVC(C=100))
-    print('=' * 20)
+    pipeline_short = make_pipeline(MinMaxScaler(), SVC(C=100))
     print("Short pipeline steps: {}".format(pipeline_short.steps))
 
     # 创建属于同一个类的多个步骤
     from sklearn.preprocessing import StandardScaler
     from sklearn.decomposition import PCA
 
-    number_title = "创建属于同一个类的多个步骤的管道"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    show_subtitle("创建属于同一个类的多个步骤的管道")
 
     pipeline = make_pipeline(StandardScaler(), PCA(n_components=2), StandardScaler())
-    print('=' * 20)
     print("Pipeline steps(same class): {}".format(pipeline.steps))
 
     # 建议使用具有明确名称的Pipeline构建，方便给每个步骤提供更具语义的名称。
 
-    from sklearn.datasets import load_breast_cancer
-    cancer = load_breast_cancer()
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
-
+    X_train, X_test, y_train, y_test = load_train_test_breast_cancer()
     pipeline.fit(X_train)
     components = pipeline.named_steps['pca'].components_
-    print('=' * 20)
-    print("pipeline.named_steps['pca']")
-    print(pipeline.named_steps['pca'])
-    print('-' * 20)
+    print("pipeline.named_steps['pca'] = ", pipeline.named_steps['pca'])
     print("pipeline.named_steps['pca'].components_.shape: {}".format(components.shape))
-    print('-' * 20)
     print("pipeline.named_steps['pca'].components_:")
     print(components)
     pass
@@ -285,33 +271,25 @@ def pipeline_attributes():
     from sklearn.preprocessing import StandardScaler
     from sklearn.linear_model import LogisticRegression
 
-    number_title = "访问管道中某个网格搜索中的属性"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    show_title("访问管道中某个网格搜索中的属性")
 
     pipeline = make_pipeline(StandardScaler(), LogisticRegression(solver='lbfgs', max_iter=10000))
     print("Pipeline steps:", pipeline.steps)
     param_grid = {'logisticregression__C': [0.01, 0.1, 1, 10, 100]}
 
-    from sklearn.datasets import load_breast_cancer
-    cancer = load_breast_cancer()
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=4)
+    X_train, X_test, y_train, y_test = load_train_test_breast_cancer()
 
     from sklearn.model_selection import GridSearchCV
     grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=5)
     grid_search.fit(X_train, y_train)
 
-    print('=' * 20)
-    print("Best estimator:")
+    show_subtitle("Best estimator")
     print(grid_search.best_estimator_)
 
-    print('=' * 20)
-    print("Logistic regression step:")
+    show_subtitle("Best estimator : Logistic regression step")
     print(grid_search.best_estimator_.named_steps['logisticregression'])
 
-    print('=' * 20)
-    print("Logistic regression coefficients:")
+    show_subtitle("Best estimator : Logistic regression coefficients")
     print(grid_search.best_estimator_.named_steps['logisticregression'].coef_)
 
     pass
@@ -321,14 +299,9 @@ def pipeline_attributes():
 # 利用管道将机器学习工作流程中的所有处理步骤封闭成一个Scikit-Learn估计器，可以使用监督任务的输出来调节预处理参数
 def adjust_preprocess_parameter():
     """管道中增加了特征生成（多项式特征）步骤"""
-    from sklearn.datasets import load_boston
-    boston = load_boston()
+    X_train, X_test, y_train, y_test = load_train_test_boston()
 
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, random_state=0)
-
-    number_title = "数据预处理+多项式特征+训练模型"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    show_title("数据预处理+多项式特征+训练模型")
 
     from sklearn.pipeline import make_pipeline
     from sklearn.preprocessing import StandardScaler
@@ -336,22 +309,25 @@ def adjust_preprocess_parameter():
     from sklearn.linear_model import Ridge
     from sklearn.model_selection import GridSearchCV
 
+    param_grid = {'ridge__alpha': [0.001, 0.01, 0.1, 1, 10, 100]}
     param_grid_poly = {'polynomialfeatures__degree': [1, 2, 3], 'ridge__alpha': [0.001, 0.01, 0.1, 1, 10, 100]}
+
+    pipeline = make_pipeline(StandardScaler(), Ridge())
     pipeline_poly = make_pipeline(StandardScaler(), PolynomialFeatures(), Ridge())
+
+    grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=5, n_jobs=3, iid=True)
     grid_search_poly = GridSearchCV(pipeline_poly, param_grid=param_grid_poly, cv=5, n_jobs=3, iid=True)
+
+    grid_search.fit(X_train, y_train)
     grid_search_poly.fit(X_train, y_train)
 
-    param_grid = {'ridge__alpha': [0.001, 0.01, 0.1, 1, 10, 100]}
-    pipeline = make_pipeline(StandardScaler(), Ridge())
-    grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=5, n_jobs=3, iid=True)
-    grid_search.fit(X_train, y_train)
+    show_subtitle("pipeline without poly features")
+    print("Best parameters :", grid_search.best_params_)
+    print("Test-set score : {:.2f}".format(grid_search.score(X_test, y_test)))
 
-    print("Best parameters with poly features:", grid_search_poly.best_params_)
-    print("Best parameters without poly features:", grid_search.best_params_)
-
-    print('=' * 20)
-    print("Test-set score with poly features: {:.2f}".format(grid_search_poly.score(X_test, y_test)))
-    print("Test-set score without poly features: {:.2f}".format(grid_search.score(X_test, y_test)))
+    show_subtitle("pipeline with poly features")
+    print("Best parameters :", grid_search_poly.best_params_)
+    print("Test-set score : {:.2f}".format(grid_search_poly.score(X_test, y_test)))
 
     plt.matshow(grid_search_poly.cv_results_['mean_test_score'].reshape(3, -1), vmin=0, cmap='viridis')
     plt.xlabel('岭回归的alpha值')
@@ -364,33 +340,31 @@ def adjust_preprocess_parameter():
 
 # 6.6 利用网格搜索和管道选择模型
 def model_selection_with_grid_pipe():
-    from sklearn.datasets import load_breast_cancer
-    cancer = load_breast_cancer()
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
-
-    number_title = "利用网格搜索和管道选择模型"
-    print('\n', '-' * 5, number_title, '-' * 5)
+    X_train, X_test, y_train, y_test = load_train_test_breast_cancer()
+    show_title("利用网格搜索和管道选择模型")
 
     from sklearn.svm import SVC
+    from sklearn.ensemble import RandomForestClassifier
     from sklearn.pipeline import Pipeline
     from sklearn.preprocessing import StandardScaler
-    # 这个设置没有用，只是为实例化管道。
+    from sklearn.preprocessing import MinMaxScaler
+    # 这里初始化为了正确实例化管道。
+    # 注1：最后一个估计器必须提供 score() 函数
+    # 注2：memory 参数提供缓存模型的路径，避免重复计算
     # pipeline = Pipeline([('preprocessing', StandardScaler()), ('classifier', SVC(gamma = 'auto'))])
-    pipeline = Pipeline([('preprocessing', None), ('classifier', SVC(gamma='auto'))])
-    from sklearn.ensemble import RandomForestClassifier
+    pipeline = Pipeline([('preprocessing', None), ('classifier', SVC(gamma='auto'))], memory='cache')
+    # pipeline = Pipeline([('preprocessing', None), ('classifier', None)])  # Error
     param_grid = [{
-        'classifier': [SVC(gamma='auto')],
-        'preprocessing': [StandardScaler(), None],
-        'classifier__gamma': [0.001, 0.01, 0.1, 1, 10, 100],
-        'classifier__C': [0.001, 0.01, 0.1, 1, 10, 100]
+            'classifier': [SVC(gamma='auto')],
+            'preprocessing': [StandardScaler(), MinMaxScaler(), None],
+            'classifier__gamma': [0.001, 0.01, 0.1, 1, 10, 100],
+            'classifier__C': [0.001, 0.01, 0.1, 1, 10, 100]
     },
-        {
-            'classifier': [RandomForestClassifier(n_estimators=100)],
-            'preprocessing': [None],
-            'classifier__max_features': [1, 2, 3]
-        }]
+            {
+                    'classifier': [RandomForestClassifier(n_estimators=100)],
+                    'preprocessing': [None],
+                    'classifier__max_features': [1, 2, 3]
+            }]
 
     from sklearn.model_selection import GridSearchCV
     grid = GridSearchCV(pipeline, param_grid=param_grid, cv=5, n_jobs=3, iid=True)
@@ -399,6 +373,7 @@ def model_selection_with_grid_pipe():
     print("Best cross-validation accuracy: {:.2f}".format(grid.best_score_))
     print("Best set score: {:.2f}".format(grid.score(X_test, y_test)))
     print("Best parameters: ", grid.best_params_)
+    print("Pipeline: ", pipeline)
     # 系统最终选择是SVC模型，而不是随机森林
 
 
@@ -407,10 +382,12 @@ def main():
     # svm_with_preprocessing_data()
     # 6.1 用预处理进行参数选择
     # parameter_selection()
+    # plot_data_preprocess()
     # 6.2 构建管道
     # construct_pipeline()
     # 6.3 在网格搜索中使用管道
     # pipeline_in_grid_search()
+    # plot_inner_preprocess()
     # 信息泄露的例子
     # information_leak()
     # 6.4 通用的管道接口
